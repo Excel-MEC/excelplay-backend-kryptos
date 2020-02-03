2a5ddcbc488a47b3213479db1b674a88b5fa014b
agzuniverse <aswinganesh666@gmail.com>
2020-02-03 22:32:51 +0530

    Use new logging and remove extra run function

--- server.go
+++ server.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"log"
+	"excelplay-backend/logger"
 	"net/http"
 	"time"
 )
 
 func main() {
-	if err := startServer(); err != nil {
-		log.Fatal(err)
-	}
-}
-
-func startServer() error {
-
 	server := &http.Server{
 		Handler:      getRouter(),
 		Addr:         ":8000",
 		WriteTimeout: 20 * time.Second,
 		ReadTimeout:  20 * time.Second,
 	}
+
+	logger.Println("Server starting on port 8000")
 	err := server.ListenAndServe()
-	return err
+	if err != nil && err != http.ErrServerClosed {
+		logger.Fatalln("Could not start server on port 8000")
+	} else {
+		logger.Fatalln(err)
+	}
 }
