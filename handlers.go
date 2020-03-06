0d4ffab4ab37956863e955d02aabcf0c5bfc382e
Aswin M Prabhu <aswinmprabhu@gmail.com>
2020-03-06 18:37:25 +0530

    Add db and logger to server struct and refactor main

--- handlers.go
+++ handlers.go
@@ -11,3 +11,14 @@ func (s *server) checkAPI(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Test"))
 }
+
+func (s *server) handleNextQuestion() http.HandlerFunc {
+	type response struct {
+		Question   string `json:"question"`
+		ImageLevel bool   `json:"image_level"`
+		LevelFile  string `json:"level_file"`
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		// do handling
+	}
+}
