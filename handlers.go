aacb40e0a04d0cab5c2b65d261feb65e84bc4f03
agzuniverse <aswinganesh666@gmail.com>
2020-03-08 22:08:56 +0530

    Rename CheckAPIIsAlive to HeartBeat

--- handlers.go
+++ handlers.go
@@ -6,10 +6,10 @@ func (s *server) handleAdmin(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Admin"))
 }
 
-// CheckAPIIsAlive sends a response back, just to check if the server is up.
-func (s *server) checkAPI(w http.ResponseWriter, r *http.Request) {
+// heartBeat sends a response back, just to check if the server is up.
+func (s *server) heartBeat(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Test"))
+	w.Write([]byte("Pong"))
 }
 
 func (s *server) handleNextQuestion() http.HandlerFunc {
