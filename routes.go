b51da2afca682f25d88905fd3c4ad7dadcd7c9ed
agzuniverse <aswinganesh666@gmail.com>
2020-03-15 22:41:49 +0530

    Selectively apply middleware to the required routes

--- routes.go
+++ routes.go
@@ -2,9 +2,7 @@ package main
 
 func (s *server) routes() {
 	s.router.HandleFunc("/admin/", s.handleAdmin).Methods("GET")
-	s.router.HandleFunc("/api/ping", s.heartBeat).Methods("GET")
-	s.router.HandleFunc("/api/question", s.handleNextQuestion()).Methods("GET")
-	s.router.HandleFunc("/api/submit", s.handleSubmission()).Methods("POST")
-
-	s.router.Use(s.authMiddleware)
+	s.router.Handle("/api/ping", s.heartBeat()).Methods("GET")
+	s.router.Handle("/api/question", s.authMiddleware(s.handleNextQuestion())).Methods("GET")
+	s.router.Handle("/api/submit", s.authMiddleware(s.handleSubmission())).Methods("POST")
 }
