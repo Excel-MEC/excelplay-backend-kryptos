be41d849662bc649059d73b8d8e77d7a41294d7e
agzuniverse <aswinganesh666@gmail.com>
2020-03-15 22:00:07 +0530

    Create auth middleware

--- routes.go
+++ routes.go
@@ -5,4 +5,6 @@ func (s *server) routes() {
 	s.router.HandleFunc("/api/ping", s.heartBeat).Methods("GET")
 	s.router.HandleFunc("/api/question", s.handleNextQuestion()).Methods("GET")
 	s.router.HandleFunc("/api/submit", s.handleSubmission()).Methods("POST")
+
+	s.router.Use(s.authMiddleware)
 }
