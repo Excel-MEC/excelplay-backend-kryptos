aa9a9692e0da1a76e2aa67dbc9fb55d819bc4687
Aswin M Prabhu <aswinmprabhu@gmail.com>
2020-03-16 13:12:53 +0530

    Add cmd dir to git

---
+++ cmd/excelplay-backend-kryptos/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"runtime"
+	"strings"
+	"time"
+
+	"github.com/Excel-MEC/excelplay-backend-kryptos/pkg/database"
+
+	"github.com/Excel-MEC/excelplay-backend-kryptos/pkg/env"
+	"github.com/Excel-MEC/excelplay-backend-kryptos/pkg/routes"
+
+	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	// if any error occurs during startup, log the error and exit with status 1
+	if err := startup(); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
+	}
+}
+func startup() error {
+	//setup logger
+	formatter := &logrus.TextFormatter{
+		TimestampFormat:        "02-01-2006 15:04:05", // the "time" field configuratiom
+		FullTimestamp:          true,
+		DisableLevelTruncation: true, // log level field configuration
+		CallerPrettyfier: func(f *runtime.Frame) (string, string) {
+			return "", fmt.Sprintf("%s:%d", formatFilePath(f.File), f.Line)
+		},
+	}
+	logrus.SetFormatter(formatter)
+	logrus.SetOutput(os.Stdout)
+
+	// read config
+	// replace hardcoding with read from config file when ready
+	config := &env.Config{
+		Host:      "db",
+		Dbport:    5432,
+		User:      "admin",
+		Password:  "password",
+		Dbname:    "db",
+		Secretkey: "supersecretkey",
+		Port:      ":8080",
+	}
+
+	//setup the database
+	db, err := database.NewDB(config)
+	if err != nil {
+		return errors.Wrap(err, "Could not setup the db")
+	}
+
+	//setup router
+	router := routes.NewRouter()
+	router.Routes(db, config)
+
+	server := &http.Server{
+		Handler:      router,
+		Addr:         config.Port,
+		WriteTimeout: 20 * time.Second,
+		ReadTimeout:  20 * time.Second,
+	}
+
+	//start server
+	logrus.Info("Server starting on port " + config.Port)
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		return errors.Wrap(err, "Could not start server on port "+config.Port)
+	}
+	return nil
+}
+
+func formatFilePath(path string) string {
+	arr := strings.Split(path, "/")
+	return arr[len(arr)-1]
+}
