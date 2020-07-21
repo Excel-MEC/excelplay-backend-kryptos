ede715eaaa4a5aa6449b320ba3413333bf35aff8
agzuniverse <aswinganesh666@gmail.com>
2020-07-21 20:37:10 +0530

    Move all queries in submission to queries file

---
+++ pkg/database/formats.go
@@ -0,0 +1,18 @@
+package database
+
+// User holds the details of a particular user
+type User struct {
+	Name      string `db:"name"`
+	CurrLevel int    `db:"curr_level"`
+}
+
+// QResponse holds the response for a question request
+type QResponse struct {
+	// Values that can be nil or a non-nullable value,
+	// such as a string are given the empty interface type
+	Number     int         `json:"number" db:"number"`
+	Question   interface{} `json:"question" db:"question"`
+	ImageLevel bool        `json:"image_level" db:"image_level"`
+	LevelFile  interface{} `json:"level_file" db:"level_file"`
+	Hints      []string    `json:"hints"`
+}
--- pkg/database/queries.go
+++ pkg/database/queries.go
@@ -1,6 +1,9 @@
 package database
 
-import "database/sql"
+import (
+	"database/sql"
+	"time"
+)
 
 // GetCurrLevel retuns the current level for the user with the given uuid.
 func (db *DB) GetCurrLevel(uuid string, currLev *int) error {
@@ -12,15 +15,9 @@ func (db *DB) CreateNewUser(uuid string, name string) (sql.Result, error) {
 	return db.Exec("insert into kuser values($1,$2,$3)", uuid, name, 1)
 }
 
-// QResponse holds the response for a question request
-type QResponse struct {
-	// Values that can be nil or a non-nullable value,
-	// such as a string are given the empty interface type
-	Number     int         `json:"number" db:"number"`
-	Question   interface{} `json:"question" db:"question"`
-	ImageLevel bool        `json:"image_level" db:"image_level"`
-	LevelFile  interface{} `json:"level_file" db:"level_file"`
-	Hints      []string    `json:"hints"`
+// GetUser gets the details of a user
+func (db *DB) GetUser(currUser *User, uuid string) error {
+	return db.Get(currUser, "select name, curr_level from kuser where id = $1", uuid)
 }
 
 // GetQuestion gets details of a certain level
@@ -31,4 +28,19 @@ func (db *DB) GetQuestion(currLev int, res *QResponse) error {
 // GetHints gets the hints released for a question
 func (db *DB) GetHints(currLev int, hints *[]string) error {
 	return db.Select(hints, "select content from hints where number = $1", currLev)
+}
+
+// LogAnswerAttempt logs every answer attempt
+func (db *DB) LogAnswerAttempt(uuid string, currUser User, answer string) (sql.Result, error) {
+	return db.Exec("insert into answer_logs values($1, $2, $3, $4)", uuid, currUser.Name, answer, time.Now())
+}
+
+// GetCorrectAns gets the correct answer for a level from the DB to check if the user's attempt is correct
+func (db *DB) GetCorrectAns(currUser User, correctAns *string) error {
+	return db.Get(correctAns, "select answer from levels where number = $1", currUser.CurrLevel)
+}
+
+// CorrectAnswerSubmitted increments the user level on submission of correct answer
+func (db *DB) CorrectAnswerSubmitted(uuid string) (sql.Result, error) {
+	return db.Exec("update kuser set curr_level = curr_level + 1 where id = $1", uuid)
 }
